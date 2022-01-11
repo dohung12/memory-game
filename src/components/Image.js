07bd8146c8595709136ee6d44f16711415aa1657
dohung <dohuyhung12@gmail.com>
2022-01-11 10:09:26 +0700

    add img caption

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Image = ({ item, onClick }) => {
-  const { img, id } = item
-  return <img src={img} id={id} alt='' className='photo' onClick={onClick} />
+  const { img, title, id } = item
+  return (
+    <figure>
+      <img
+        src={img}
+        data-testid={id}
+        alt={title}
+        className='photo'
+        onClick={onClick}
+      />
+      <h3>{title}</h3>
+    </figure>
+  )
 }
 
 export default Image
